e1f2cb8a63e08d9769d28af3b738727f371c1e53
Ryan Staples <rstaples@keeta.com>
2025-10-07 11:25:33 -0400

    cleanup from PR review

--- src/anchor/fx-service.ts
+++ src/anchor/fx-service.ts
@@ -5,193 +5,234 @@
  */
 
 import * as KeetaAnchor from '@keetanetwork/anchor';
-import { debugPrintableObject, getBaseTokenDecimals, getFaucetTokens, waitForResult } from '../helper.js';
+import { debugPrintableObject, getBaseTokenDecimals, getFaucetTokens } from '../helper.js';
 import { KeetaNetFXAnchorHTTPServer } from '@keetanetwork/anchor/services/fx/server.js';
+import type { TokenAddress } from '@keetanetwork/keetanet-client/lib/account.js';
 import * as util from 'util';
 
 const Account = KeetaAnchor.KeetaNet.lib.Account;
 
 const DEBUG = false;
-const logger = DEBUG ? console : undefined;
+const logger = DEBUG ? { logger: console } : {};
 const network = 'test';
 
 async function main() {
-    // Setup Accounts to test
-    const seed = Account.generateRandomSeed({ asString: true});
-    console.debug(`Seed: ${seed}`);
-    const account = Account.fromSeed(seed, 0);
-    console.debug(`Account: ${account.publicKeyString.get()}`);
-    const liquidityProvider = Account.fromSeed(seed, 1);
-    console.debug(`Liquidity FX Provider: ${liquidityProvider.publicKeyString.get()}`);
+	/**
+	 * Generate a random seed to base the accounts on, these accounts are
+	 * `userAccount` is a user account that creates two tokens and can perform swaps with the FX Anchor
+	 * `liqudityProvider` is the account which holds the FX Anchor liquidity
+	 */
+	const seed = Account.generateRandomSeed({ asString: true });
+	const userAccount = Account.fromSeed(seed, 0);
+	const liquidityProvider = Account.fromSeed(seed, 1);
 
-    // Create UserClient for TEST network
-    const userClient = KeetaAnchor.KeetaNet.UserClient.fromNetwork(network, account);
+	console.log(`Seed: ${seed}`);
+	console.log(`User Account: ${userAccount.publicKeyString.get()}`);
+	console.log(`Liquidity FX Provider: ${liquidityProvider.publicKeyString.get()}`);
 
-    // Get 10 Tokens from the Faucet for fees
-    await getFaucetTokens(account);
+	// Create UserClient for Network and User Account
+	await using userClient = KeetaAnchor.KeetaNet.UserClient.fromNetwork(network, userAccount);
 
-    // Wait for faucet distribution
-    const balanceResult = await waitForResult(async function() {
-        const balance = await userClient.balance(userClient.baseToken);
-        return(balance > 0n);
-    });
-    if (!balanceResult) {
-        throw(new Error('Did not get faucet balance within 15 seconds'));
-    }
+	const baseTokenDecimals = await getBaseTokenDecimals(network);
+	if (baseTokenDecimals === null) {
+		throw(new Error(`Failed to get Base Token Decimals for Network: ${network}`));
+	}
 
-    // Create 2 tokens for FX
-    const { account: token1 } = await userClient.generateIdentifier(Account.AccountKeyAlgorithm.TOKEN);
-    const { account: token2 } = await userClient.generateIdentifier(Account.AccountKeyAlgorithm.TOKEN);
-    if (!token1.isToken() || !token2.isToken()) {
-        // basic type narrowing for TOKEN
-        throw(new Error('Tokens Should be TOKEN Key Algorithm'));
-    }
-    console.debug(`$TKNA Token: ${token1.publicKeyString.get()}`);
-    console.debug(`$TKNB Token: ${token2.publicKeyString.get()}`);
+	// Get Tokens from the Faucet for Fees
+	const faucetRequest = await getFaucetTokens(userAccount, network);
 
-    const initialTokenSupply = 50_000n;
-    const builder = userClient.initBuilder();
-    for (const token of [{ token: token1, symbol: 'TKNA' }, { token: token2, symbol: 'TKNB' }]) {
-        // Set token info and metadata with base permission of ACCESS for anyone to use the token
-        const basicMetadata = Buffer.from(JSON.stringify({ decimalPlaces: 6 })).toString('base64');
-        builder.setInfo({
-            name: token.symbol,
-            description: `Example Token ${token.symbol}`,
-            metadata: basicMetadata,
-            defaultPermission: new KeetaAnchor.KeetaNet.lib.Permissions(['ACCESS'])
-        }, { account: token.token });
+	if (!faucetRequest) {
+		throw(new Error('Failed to get Faucet Tokens'));
+	}
 
-        // Setup Initial Token Supplies and Distribute Amounts to Liquidity Pool
-        builder.modifyTokenSupply(initialTokenSupply, { account: token.token });
-        // Compute blocks to ensure token supply is added before sending
-        await builder.computeBlocks();
-        builder.send(liquidityProvider, 10_000n, token.token, undefined, { account: token.token });
-    }
+	/**
+	 * Create 2 tokens for the FX Anchor to operate on, 1 to swap for the other
+	 * TKNA: The token the user account initially has
+	 * TKNB: The token the user account wants to receive
+	 */
+	const { account: token1 } = await userClient.generateIdentifier(Account.AccountKeyAlgorithm.TOKEN);
+	const { account: token2 } = await userClient.generateIdentifier(Account.AccountKeyAlgorithm.TOKEN);
+	if (!token1.isToken() || !token2.isToken()) {
+		// basic type narrowing for AccountKeyAlgorithm.TOKEN
+		throw(new Error('Tokens Should be TOKEN Key Algorithm'));
+	}
 
-    // Compute blocks again before sending to account and liquidity
-    await builder.computeBlocks();
-    // Send some TKN1 Supply to user account
-    builder.send(account, 10_000n, token1, undefined, { account: token1});
+	const initialTokenSupply = 50_000n;
 
-    // Send some KTA Base Token to the liquidity provider so it can pay fees
-    builder.send(liquidityProvider, 2n * (10n ** BigInt(await getBaseTokenDecimals(network))), userClient.baseToken);
+	// Create a builder to construct the blocks
+	const builder = userClient.initBuilder();
 
-    // Compute Final Blocks
-    await builder.computeBlocks();
-    console.log(`Token Setup Blocks: ${[builder.blocks.map(block => block.hash.toString())]}`);
+	// Set token info and metadata with base permission of ACCESS for anyone to use the token
+	const basicMetadata = Buffer.from(JSON.stringify({ decimalPlaces: 6 }), 'utf-8').toString('base64');
+	for (const tokenInfo of [{ token: token1, name: 'TKNA' }, { token: token2, name: 'TKNB' }]) {
+		builder.setInfo({
+			name: tokenInfo.name,
+			description: `Example Token ${tokenInfo.name}`,
+			metadata: basicMetadata,
+			defaultPermission: new KeetaAnchor.KeetaNet.lib.Permissions(['ACCESS'])
+		}, { account: tokenInfo.token });
 
-    // Publish builder blocks to the network
-    await builder.publish();
+		// Setup Initial Token Supplies and Distribute Amounts to Liquidity Pool
+		builder.modifyTokenSupply(initialTokenSupply, { account: tokenInfo.token });
 
-    // Setup FX Anchor HTTP Server
-    await using fxServer = new KeetaNetFXAnchorHTTPServer({
-        ...(logger ? { logger: logger } : {}),
+		/**
+		 * Compute blocks to ensure token supply is added before sending.
+		 * The builder does not always guarantee correct order so we compute the supply blocks to ensure they come first
+		 */
+		await builder.computeBlocks();
+		builder.send(liquidityProvider, 10_000n, tokenInfo.token, undefined, { account: tokenInfo.token });
+	}
+
+	// Send some TKNA Supply to user account
+	builder.send(userAccount, 10_000n, token1, undefined, { account: token1 });
+
+	// Send some KTA Base Token to the liquidity provider so it can pay fees
+	builder.send(liquidityProvider, 2n * (10n ** BigInt(baseTokenDecimals)), userClient.baseToken);
+
+	// Compute Final Blocks
+	await builder.computeBlocks();
+
+	console.log(`$TKNA Token: ${token1.publicKeyString.get()}`);
+	console.log(`$TKNB Token: ${token2.publicKeyString.get()}`);
+	console.log(`Token Setup Blocks: ${[builder.blocks.map(block => block.hash.toString())]}`);
+
+	// Publish builder blocks to the network
+	await builder.publish();
+
+	await using liquidityProviderUserClient = KeetaAnchor.KeetaNet.UserClient.fromNetwork(network, liquidityProvider);
+	// Setup FX Anchor HTTP Server
+	await using fxServer = new KeetaNetFXAnchorHTTPServer({
+		...logger,
 		account: liquidityProvider,
-        quoteSigner: liquidityProvider,
-		client: { client: userClient.client, network: userClient.config.network, networkAlias: userClient.config.networkAlias },
+		quoteSigner: liquidityProvider,
+		client: liquidityProviderUserClient,
 		fx: {
-            // Provided function to calculate the rate
+			// Provided function to calculate the rate
 			getConversionRateAndFee: async function(request) {
 				let rate = 0.88;
-                // Affinity could be 'from' or 'to' and can change which direction the rate should be calculated
+				// Affinity could be 'from' or 'to' and can change which direction the rate should be calculated
 				if (request.affinity === 'to') {
 					rate = 1 / rate;
 				}
+				/**
+				 * Convert the request amount to bigint
+				 * Multiple the rate by the number of decimals (6 in this example) for the token so we can do bigint math
+				 */
+				const fixedDecimalRate = (rate * (10 ** 6)).toFixed(0);
+				const convertedAmount = (BigInt(request.amount) * BigInt(fixedDecimalRate)) / BigInt((10 ** 6));
 				return({
 					account: liquidityProvider.publicKeyString.get(),
-					convertedAmount: (parseInt(request.amount) * rate).toFixed(0),
+					convertedAmount: convertedAmount.toString(),
 					cost: {
-                        amount: '0',
-                        token: token1.publicKeyString.get()
-                    }
+						amount: '0',
+						token: token1.publicKeyString.get()
+					}
 				});
 			}
 		}
 	});
 
-    // Start the HTTP Server - getEstimate, getQuote, createExchange and getExchangeStatus endpoints are already defined in the KeetaNetFxAnchorHTTPServer class
-    await fxServer.start();
+	// Start the HTTP Server - getEstimate, getQuote, createExchange and getExchangeStatus endpoints are already defined in the KeetaNetFxAnchorHTTPServer class
+	await fxServer.start();
 	const fxServerURL = fxServer.url;
 
-    // Set the Anchor Root Metadata
-    await userClient.setInfo({
-        description: 'FX Anchor Test Root',
+	// Set the Anchor Root Metadata
+	await userClient.setInfo({
+		description: 'FX Anchor Test Root',
 		name: 'TEST',
 		metadata: KeetaAnchor.lib.Resolver.Metadata.formatMetadata({
-            version: 1,
-            currencyMap: {
-                '$TKNA': token1.publicKeyString.get(),
-                '$TKNB': token2.publicKeyString.get()
-            },
-            services: {
-                fx: {
-                    Test: {
-                        from: [
-                            {
-                                currencyCodes: [token1.publicKeyString.get()],
-                                to: [token2.publicKeyString.get()]
-                            },
-                            {
-                                currencyCodes: [token2.publicKeyString.get()],
-                                to: [token1.publicKeyString.get()]
-                            }
-                        ],
-                        operations: {
-                            getEstimate: `${fxServerURL}/api/getEstimate`,
-                            getQuote: `${fxServerURL}/api/getQuote`,
-                            createExchange: `${fxServerURL}/api/createExchange`,
-                            getExchangeStatus: `${fxServerURL}/api/getExchangeStatus/{exchangeID}`
-                        }
-                    }
-                }
-            }
-        })
-    });
+			version: 1,
+			currencyMap: {
+				'$TKNA': token1.publicKeyString.get(),
+				'$TKNB': token2.publicKeyString.get()
+			},
+			services: {
+				fx: {
+					Test: {
+						from: [
+							{
+								currencyCodes: [token1.publicKeyString.get()],
+								to: [token2.publicKeyString.get()]
+							},
+							{
+								currencyCodes: [token2.publicKeyString.get()],
+								to: [token1.publicKeyString.get()]
+							}
+						],
+						operations: {
+							getEstimate: `${fxServerURL}/api/getEstimate`,
+							getQuote: `${fxServerURL}/api/getQuote`,
+							createExchange: `${fxServerURL}/api/createExchange`,
+							getExchangeStatus: `${fxServerURL}/api/getExchangeStatus/{exchangeID}`
+						}
+					}
+				}
+			}
+		})
+	});
 
-    // FX Client to interact with FX HTTP Server
-    const fxClient = new KeetaAnchor.FX.Client(userClient, {
-		root: account,
-		signer: account,
-		account: account,
-        ...(logger ? { logger: logger } : {})
-    });
+	// FX Client to interact with FX HTTP Server
+	const fxClient = new KeetaAnchor.FX.Client(userClient, {
+		root: userAccount,
+		signer: userAccount,
+		account: userAccount,
+		...logger
+	});
 
-    const initialBalances = await userClient.allBalances();
-    console.debug(`Initial User Balances: ${util.inspect(debugPrintableObject(initialBalances), { depth: 4, colors: true })}`);
+	// Get initial balances for User Account before the swap occurs to compare
+	const initialBalances = await userClient.allBalances();
 
-    // getQuotes returns any FX providers that can fulfil the requested swap
-    const fxQuoteWithProviders = await fxClient.getQuotes(
-        { from: token1, to: token2, amount: 100, affinity: 'from' }
-    );
-    if (fxQuoteWithProviders === null) {
-        throw(new Error('Failed to get FX Providers for Exchange'));
-    }
+	// Details of the swap request we want to make
+	const swapRequest: { from: TokenAddress, to: TokenAddress, amount: string, affinity: 'from' | 'to' } = {
+		// Token we want to send
+		from: token1,
+		// Token we want to receive
+		to: token2,
+		// Amount to exchange.  Token is determined from `affinity`
+		amount: '100',
+		// Direction of the exchange and what the amount should apply too
+		affinity: 'from'
+	}
 
-    // In this example there is only 1 FX provider, so pick the first one
-    const fxQuoteProvider = fxQuoteWithProviders[0];
-    console.debug(`Exchange Quote: ${util.inspect(debugPrintableObject(fxQuoteProvider.quote), { depth: 4, colors: true })}`);
+	// getQuotes returns any FX providers that can fulfil the requested swap
+	const fxQuoteWithProviders = await fxClient.getQuotes(swapRequest);
+	if (fxQuoteWithProviders === null) {
+		throw(new Error('Failed to get FX Providers for Exchange'));
+	}
 
-    // Create the swap request block.  Defined amount being sent from our account
-    const from = { account: account, token: token1, amount: 100n };
+	// In this example there is only 1 FX provider, so pick the first one
+	const fxQuoteProvider = fxQuoteWithProviders[0];
+	if (fxQuoteProvider === undefined) {
+		throw(new Error('FX Provider is undefined'));
+	}
+	const quote = fxQuoteProvider.quote;
 
-    // Define amount to be received/swapped with the FX provider using the converted amount provided in the quote
-    const to = { account: Account.fromPublicKeyString(fxQuoteProvider.quote.account), token: token2, amount: BigInt(fxQuoteProvider.quote.convertedAmount) };
+	// Create the swap request block.  Defined amount being sent from our account
+	const from = { account: userAccount, token: swapRequest.from, amount: BigInt(swapRequest.amount) };
 
-    // Create the swap block.
-    // This is optional and will be handled by the FX Client automatically if the block is not provided using the intil quote request parameters
-    const createSwapBlock = await userClient.createSwapRequest({ from, to });
-    await fxQuoteProvider.createExchange(createSwapBlock);
+	// Define amount to be received/swapped with the FX provider using the converted amount provided in the quote
+	const to = { account: Account.fromPublicKeyString(quote.account), token: swapRequest.to, amount: BigInt(quote.convertedAmount) };
+	/**
+	 * Create the swap block.
+	 * This is optional and will be handled by the FX Client automatically
+	 * If the block is not provided, the FX Client will use initial quote request parameters to create one
+	 */
+	const createSwapBlock = await userClient.createSwapRequest({ from, to });
+	await fxQuoteProvider.createExchange(createSwapBlock);
 
-    const finalBalances = await userClient.allBalances();
-    console.debug(`Final User Balances: ${util.inspect(debugPrintableObject(finalBalances), { depth: 4, colors: true })}`);
+	// Get final balances to compare after swap
+	const finalBalances = await userClient.allBalances();
+	const liquidityBalances = await userClient.client.getAllBalances(liquidityProvider);
 
-    const liquidityBalances = await userClient.client.getAllBalances(liquidityProvider);
-    console.debug(`Liquidity Provider Final Balances: ${util.inspect(debugPrintableObject(liquidityBalances), { depth: 4, colors: true })}`);
+	console.log(`Exchange Quote: ${util.inspect(debugPrintableObject(fxQuoteProvider.quote), { depth: 4, colors: true })}`);
+	console.log(`Initial User Balances: ${util.inspect(debugPrintableObject(initialBalances), { depth: 4, colors: true })}`);
+	console.log(`Final User Balances: ${util.inspect(debugPrintableObject(finalBalances), { depth: 4, colors: true })}`);
+	console.log(`Liquidity Provider Final Balances: ${util.inspect(debugPrintableObject(liquidityBalances), { depth: 4, colors: true })}`);
 }
 
 main().then(function() {
 	process.exit(0);
-}, function(err) {
+}, function(err: unknown) {
 	console.error(err);
 	process.exit(1);
 });
--- src/helper.ts
+++ src/helper.ts
@@ -1,49 +1,76 @@
 import * as KeetaNet from '@keetanetwork/keetanet-client';
 import type { Account } from '@keetanetwork/keetanet-client/lib/account.js';
+import type { Networks } from '@keetanetwork/keetanet-client/config/index.js';
 
 const debugPrintableObject = KeetaNet.lib.Utils.Helper.debugPrintableObject;
 
-export async function getBaseTokenDecimals(network: 'test' | 'main'): Promise<number> {
-    const userClient = KeetaNet.UserClient.fromNetwork(network, null);
-    const baseTokenInfo = await userClient.client.getAccountInfo(userClient.baseToken);
-    const tokenMetadata = JSON.parse(Buffer.from(baseTokenInfo.info.metadata, 'base64').toString());
-    return(tokenMetadata['decimalPlaces']);
-}
-
-export async function getFaucetTokens(acct: Account | string) {
-    const account = typeof acct === 'string' ? acct : acct.publicKeyString.get()
-    try {
-        const params = new URLSearchParams();
-        params.append('address', account);
-        params.append('amount', '10');
-        const response = await fetch("https://faucet.test.keeta.com", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: params.toString()
-        });
-
-        if (response.ok) {
-            console.log(`Requesting Tokens from Faucet for: ${account}`);
-        } else {
-            console.error(`Faucet Request Failed for: ${account}`);
-        }
-    } catch (error) {
-        console.error(`Faucet Request Failed for: ${account} `, error);
-    }
+export async function getBaseTokenDecimals(network: Networks): Promise<number | null> {
+	const userClient = KeetaNet.UserClient.fromNetwork(network, null);
+	const baseTokenInfo = await userClient.client.getAccountInfo(userClient.baseToken);
+	const tokenMetadata: unknown = JSON.parse(Buffer.from(baseTokenInfo.info.metadata, 'base64').toString());
+	if (tokenMetadata && typeof tokenMetadata === 'object' && 'decimalPlaces' in tokenMetadata && typeof tokenMetadata.decimalPlaces === 'number') {
+		return(tokenMetadata.decimalPlaces)
+	}
+	return(null);
 }
 
 export async function waitForResult(code: () => Promise<boolean>, timeout = 10000) {
-    let result = await code();
-    const startTime = Date.now();
-    while(!result && (startTime + timeout > Date.now())) {
-        await KeetaNet.lib.Utils.Helper.asleep(50);
-        result = await code();
-    }
-    return(result);
+	for (const startTime = Date.now(); startTime + timeout > Date.now();) {
+		const result = await code();
+		if (result) {
+			return(true);
+		}
+		await KeetaNet.lib.Utils.Helper.asleep(50);
+	}
+	return(false);
+}
+
+export async function getFaucetTokens(acct: Account | string, network: Networks) {
+	if (network !== 'test') {
+		throw(new Error('Faucet is Only Available on the Test Network'));
+	}
+
+	let account: string;
+	if (typeof acct === 'string') {
+		account = acct;
+	} else {
+		account = acct.publicKeyString.get();
+	}
+
+	await using tempUserClient = KeetaNet.UserClient.fromNetwork(network, null);
+	const initialBalance = await tempUserClient.client.getBalance(account, tempUserClient.baseToken);
+
+	// Make a request to the Faucet for KTA tokens
+	try {
+		const params = new URLSearchParams();
+		params.append('address', account);
+		params.append('amount', '5');
+		const response = await fetch("https://faucet.test.keeta.com", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: params.toString()
+		});
+
+		if (response.ok) {
+			console.log(`Requesting Tokens from Faucet for: ${account}`);
+		} else {
+			console.error(`Faucet Request Failed for: ${account}`);
+		}
+	} catch (error) {
+		console.error(`Faucet Request Failed for: ${account} `, error);
+	}
+
+	// Wait for the account balance to be updated
+	const balanceResult = await waitForResult(async function() {
+		const balance = await tempUserClient.client.getBalance(account, tempUserClient.baseToken);
+		return(balance >= (initialBalance + 5n));
+	});
+
+	return(balanceResult);
 }
 
 export {
-    debugPrintableObject
+	debugPrintableObject
 };
